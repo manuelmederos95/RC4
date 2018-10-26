c966f2dad02e0dc9aa57d32548d7001369b2e8db
juan <jm.anghileri@wanadoo.fr>
2018-10-26 11:21:36 -0700

    [Structure] Ajout des doosiers pour la TDD

---
+++ test/test_main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by juan on 26/10/18.
+//
+
+#include "test_main.h"
---
+++ test/test_main.h
@@ -0,0 +1,14 @@
+//
+// Created by juan on 26/10/18.
+//
+
+#ifndef RC3_TEST_MAIN_H
+#define RC3_TEST_MAIN_H
+
+
+class test_main {
+
+};
+
+
+#endif //RC3_TEST_MAIN_H
